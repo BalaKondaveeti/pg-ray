d344e361fa4b4141ab78e7fed16881322d95893f
BalaKondaveeti <bkondave@asu.edu>
2025-03-05 12:34:44 -0800

    Init Project
    tailwindcss, vite

---
+++ commands.sh
@@ -0,0 +1,6 @@
+# use these commands to create the workspace
+
+npm create vite@latest pg-ray -- --template react
+cd pg-ray
+npm install reactflow lucide-react clsx tailwind-merge
+npm install -D tailwindcss postcss autoprefixer
